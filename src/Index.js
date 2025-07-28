0388b0382c594f07a6fadb93f81ad4c0e06ec161
LR-OM <Lauraromero772@gmail.com>
2025-07-27 20:11:45 -0500

    fixed my context

--- src/Index.js
+++ src/Index.js
@@ -13,7 +13,7 @@ function generatePoem (event){
 
   let instructionsInput=document.querySelector("#user-instructions");
   let apiKey="ff43f96f1et38ea9470b0c722o5c6709";
-  let context="You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML, example <p>. Make sure to follow the user instructions and seperate each line with a <br/>. Sign at the end of the poem with a `shecodes AI`inside a <strong> element."; 
+  let context="You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem. Make sure to follow the user instructions. Type poem in basic HTML, example <p> and seperate each line with a <br/>. Sign at the end of the poem with a `shecodes AI`inside a <strong> element."; 
   let prompt= `user instructions: Generate a French poem about ${instructionsInput.Value}`;
   let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
